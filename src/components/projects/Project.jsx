2b4237957e915dcf5c3073b6fabd31885b413c04
yunamoon <tpwkao0051@gmail.com>
2024-03-13 01:05:43 +0900

    card list update

--- src/components/projects/Project.jsx
+++ src/components/projects/Project.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 function ProjectItem ({title , contents}) {
 
   return (
-  <>
-  <h4 className='text-1xl font-semibold mt-8' >{title}</h4> 
-  <p className='text-gray-400 mt-2'>{contents}</p></>
+  <div className='h-[100px'>
+  <h4 className='text-1xl font-semibold mt-3 ' >{title}</h4> 
+  <p className='text-gray-400 mt-1'>{contents}</p></div>
   );
 
 }
@@ -19,25 +19,27 @@ function Project({project}) {
 
   return (
    
-          <div className='flex flex-col w-1/2  sm:gap-10 border border-gray-500 rounded-md p-6 transform transition duration-300 cursor-pointer'>
+          <div className='flex flex-col w-full sm:w-1/2 h-full'>
 
-                  <img className='w-[100px] h-full' src={project.img} />
+          <div className='m-5 border border-gray-500 rounded-md p-6 hover:scale-105 transform transition duration-300 cursor-pointer'>
+               
+                <img className='w-full mb-5' src={project.img} />
       
 
                 <div className='w-full'>
-                <h3 className='text-2xl font-semibold mt-8' >스마트 쑥쑥 웹</h3> 
-               
-                <ProjectItem title={'Description.'} contents={'신규 서비스인 스마트 쑥쑥의 초기 기획과 프로젝트 설계에 참여하고 베타 버전의 프론트엔드 영역을 개발했습니다.'}/>
-                <ProjectItem title={'What did I do'} contents={(<>▷ 스마트 쑥쑥의 핵심 컨셉인 실시간 모니터링 화면 구현 <br/> ▷ 그래프를 추가해 농장 환경 데이터 시각화</>)}/>
-                <ProjectItem title={'Tech Stack.'} contents={'Angular , Angular-Material, Chart.Js,  Firebase, Git actions'}/>             
-                
-                <div className='flex mt-8 gap-3'>
-                <button onClick={()=> openExternalPage(project.liveLink)} className='text-sm flex-1 py-3  w-full bg-gradient-to-t from-yellow-600 rounded-full to-yellow-300 hover:from-yellow-700 hover:to-yellow-500'>Live Demo</button>
-                <button onClick={()=>openExternalPage(project.gitLink)} className='ext-sm flex-1  py-3 w-full border rounded-full hover:border-yellow-300 hover:text-yellow-300'>Github</button>
-                </div>
-              
+                  <h3 className='text-2xl font-semibold mb-5' >{project.title}</h3> 
+                  
+                  <ProjectItem  title={'Description.'} contents={project.Description}/>
+                  {/* <ProjectItem title={'What did I do'} contents={(<>▷ 스마트 쑥쑥의 핵심 컨셉인 실시간 모니터링 화면 구현 <br/> ▷ 그래프를 추가해 농장 환경 데이터 시각화</>)}/> */}
+                  <ProjectItem   title={'Tech Stack.'} contents={project.TechStack}/>             
+                  
+                  <div className='flex mt-8 gap-3'>
+                  <button onClick={()=> openExternalPage(project.liveLink)} className='text-sm flex-1 py-3  w-full bg-gradient-to-t from-yellow-600 rounded-full to-yellow-300 hover:from-yellow-700 hover:to-yellow-500'>Live Demo</button>
+                  <button onClick={()=>openExternalPage(project.gitLink)} className='ext-sm flex-1  py-3 w-full border rounded-full hover:border-yellow-300 hover:text-yellow-300'>Github</button>
+                  </div>
               </div>
       
+            </div>
             </div>
 
   )
--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -13,21 +13,24 @@ function Projects() {
     const [projects , setProjects] = useState([
         {id : 1,
         title : 'Commune', 
-        contents : 'Commune은 이미지를 공유하고 상호 작용할 수 있는 온라인 커뮤니티 플랫폼입니다. React를 기반으로하여 사용자들은 서로의 게시글에 좋아요를 표시하고 댓글을 남기며 친목을 형성할 수 있습니다.' , 
+        Description :  '신규 서비스인 스마트 쑥쑥의 초기 기획과 프로젝트 설계에 참여하고 베타 버전의 프론트엔드 영역을 개발했습니다.',
+        TechStack  : 'TypeScript, JavaScript, React, Redux, styled-components, Tailwind css, Firebase, Jest, Git actions',
         img : 'src/assets/images/sample.jpg',
         liveLink : 'https://hh99-ac7ee--pr2-develop-hl070utt.web.app',
         gitLink : 'https://github.com/yunamoon/react-community'},
         
-        {id : 3,
+        {id : 2,
         title : '오늘의 위로', 
-        contents : '오늘의 위로는 React 기반의 프로젝트로, ChatGPT API를 활용하여 용자가 일상에서 받는 고민이나 스트레스를 공유하고 위로를 받을 수 있는 서비스입니다.' , 
+        Description :  'Chat GPT API를 활용하여 유저가 일기를 남기면 하루 일과를 분석하여 그에 상응하는 공감과 피드백을 제공하는 서비스입니다.',
+        TechStack  : 'TypeScript, JavaScript, React, Redux, Chat GPT API, Tailwind css, Firebase, Jest, Netlify',
         img : 'src/assets/images/sample.jpg',  
         liveLink : 'https://hh99-ac7ee--pr2-develop-hl070utt.web.app',
         gitLink : 'https://github.com/yunamoon/react-community'},
 
-        {id : 2,
+        {id : 3,
         title : '펫파크', 
-        contents : '펫파크는 React로 구현된 애견 용품의 온라인 쇼핑 플랫폼입니다. 판매 인증을 받은 사용자들은 안전하고 신뢰성 있는 제품을 판매하며, 고객들은 다양한 기술로 제작된 제품을 구매할 수 있습니다.' , 
+        Description :  '펫파크는 React로 구현된 애견 용품의 온라인 쇼핑 플랫폼입니다. 판매 인증을 받은 사용자들은 안전하고 신뢰성 있는 제품을 판매하며, 고객들은 다양한 기술로 제작된 제품을 구매할 수 있습니다.',
+        TechStack  : 'TypeScript, JavaScript, React, Redux, Chat GPT API, Tailwind css, Firebase, Jest, Netlify',
         img : 'src/assets/images/sample.jpg',
         liveLink : 'https://hh99-ac7ee--pr2-develop-hl070utt.web.app',
         gitLink : 'https://github.com/yunamoon/react-community'},
@@ -35,33 +38,43 @@ function Projects() {
         
         {id : 4,
         title : '나무늘보의 서재', 
-        contents : '나무늘보의 서재는 Next.js를 기반의 프로젝트로 저의 학습 여정을 기록하고, 참고 자료와 학습 내용을 공유하는 공간입니다.' , 
-        img : 'src/assets/images/sample.jpg'},
+        Description :  '나무늘보의 서재는 Next.js를 기반의 블로그 프로젝트로 저의 학습 여정을 기록하고, 참고 자료와 학습 내용을 공유하는 공간입니다.',
+        TechStack  : 'TypeScript, Next.js, Redux, Tailwind css, Firebase, Jest, Netlify',
+        img : 'src/assets/images/sample.jpg',
+        liveLink : 'https://hh99-ac7ee--pr2-develop-hl070utt.web.app',
+        gitLink : 'https://github.com/yunamoon/react-community'},
         
         {id : 5,
         title : '스마트 쑥쑥 웹', 
-        contents : '스마트 쑥쑥은 농장 환경 정보를 실시간으로 모니터링하고 제공하는 웹 서비스입니다. 리액트를 사용하여 만들어졌으며, Firebase와 Google Cloud Platform을 활용하여 사용자들에게 쉽고 편리한 환경 정보 솔루션을 제공합니다.' , 
-        img : 'src/assets/images/sample.jpg'},
+        Description :  '신규 서비스인 스마트 쑥쑥의 초기 기획과 프로젝트 설계에 참여하고 베타, 초기 버전, 마이그레이션 중 프론트엔드 영역을 개발했습니다.',
+        TechStack  : 'Angular , Angular-Material, Chart.Js, TypeScript, JavaScript, React, Redux, Redux Saga, Sass, styled-components, Tailwind css, GCP, Firebase , Jest, Git actions',
+        img : 'src/assets/images/sample.jpg',
+        liveLink : 'https://hh99-ac7ee--pr2-develop-hl070utt.web.app',
+        gitLink : 'https://github.com/yunamoon/react-community'},
 
-        {id : 6,
-        title : '스마트 쑥쑥 앱', 
-        contents : '스마트 쑥쑥 앱은 플러터를 사용하여 개발된 모바일 애플리케이션으로, 농장 환경 정보를 실시간으로 모니터링하고 사용자에게 제공합니다. Firebase와 Google Cloud Platform을 통해 데이터를 관리하여 사용자들이 편리하게 정보를 얻을 수 있습니다.' , 
-        img : 'src/assets/images/sample.jpg'},
+        // {id : 6,
+        // title : '스마트 쑥쑥 앱', 
+        // contents : '스마트 쑥쑥 앱은 플러터를 사용하여 개발된 모바일 애플리케이션으로, 농장 환경 정보를 실시간으로 모니터링하고 사용자에게 제공합니다. Firebase와 Google Cloud Platform을 통해 데이터를 관리하여 사용자들이 편리하게 정보를 얻을 수 있습니다.' , 
+        // Description :  '신규 서비스인 스마트 쑥쑥의 초기 기획과 프로젝트 설계에 참여하고 베타 버전의 프론트엔드 영역을 개발했습니다.',
+        // TechStack  : 'Angular , Angular-Material, Chart.Js,  Firebase, Git actions',
+        // img : 'src/assets/images/sample.jpg',
+        // liveLink : 'https://hh99-ac7ee--pr2-develop-hl070utt.web.app',
+        // gitLink : 'https://github.com/yunamoon/react-community'},
     
     ])
 
 
         return (
         <section className='py-8'>
-            <div className='container m-auto px-4 py-9 bg-gray-300'>
-            <h2 className='text-2xl font-semibold mb-10'>Projects</h2>
-            <div className='flex flex-wrap justify-between'> 
-            {projects.map((item) => (
+            <div className='container m-auto px-4 py-9'>
+            <h2 className='text-2xl font-semibold mb-5'>Projects</h2>
+            <div className='flex flex-wrap flex-col sm:flex-row '> 
+           {projects.map((item) => (
                  <Project key={item.id} project={item} />))}
             </div>
           
             {/* <Swiper
-              className='container flex items-center mt-10 '
+              className=''
               effect={'coverflow'}
               grabCursor={true}
               centeredSlides={true}
